1e2d77b073a477de1ca35890cc336fa7e8047ca3
Jan Dvorak <storytellercz@proton.me>
2025-04-01 13:32:44 +0200

    Bump packages in prep for release

--- plugins/bundler/package.js
+++ plugins/bundler/package.js
@@ -1,7 +1,7 @@
 /* eslint-disable prefer-arrow-callback */
 Package.describe({
     name: 'communitypackages:meteor-desktop-bundler',
-    version: '3.3.0',
+    version: '4.0.0-rc.0',
     summary: 'Bundles .desktop dir into desktop.asar.',
     git: 'https://github.com/Meteor-Community-Packages/meteor-desktop',
     documentation: 'README.md'
@@ -9,13 +9,13 @@ Package.describe({
 
 Package.registerBuildPlugin({
     name: 'meteor-desktop-bundler',
-    use: ['ecmascript@0.16.1'],
+    use: ['ecmascript@0.16.6'],
     sources: ['bundler.js'],
     npmDependencies: { chokidar: '3.5.3' }
 });
 
 Package.onUse(function onUse(api) {
-    api.versionsFrom('1.4.4.6');
+    api.versionsFrom('1.12.1');
     api.use('isobuild:compiler-plugin@1.0.0');
     api.addFiles([
         'version._desktop_.js'
