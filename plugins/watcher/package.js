2b4453a0000f5ece036ff9a300587e00b2122a79
Andreas West <a4xrbj1@gmail.com>
2026-02-05 21:29:18 +0800

    chore: bump version to 4.0.2

--- plugins/watcher/package.js
+++ plugins/watcher/package.js
@@ -1,7 +1,7 @@
 /* eslint-disable prefer-arrow-callback */
 Package.describe({
     name: 'communitypackages:meteor-desktop-watcher',
-    version: '4.0.0-rc.1',
+    version: '4.0.2',
     summary: 'Watches .desktop dir and triggers rebuilds on file change.',
     git: 'https://github.com/Meteor-Community-Packages/meteor-desktop',
     documentation: 'README.md',
@@ -16,7 +16,7 @@ Package.onUse(function onUse(api) {
     api.versionsFrom('METEOR@3.0');
     api.use('ecmascript');
     api.use([
-        'communitypackages:meteor-desktop-bundler@4.0.0-rc.1',
+        'communitypackages:meteor-desktop-bundler@4.0.2',
     ], ['server'], {
         weak: true
     });
